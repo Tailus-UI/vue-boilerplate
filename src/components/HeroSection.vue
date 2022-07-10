c0cbb9fedd409ef2008528b0a604516173b1a02b
Meschac Irung <meschacirung@gmail.com>
2022-07-10 14:35:46 +0200

    setup project

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="relative">
+        <div class="absolute inset-x-0 top-0 flex items-center justify-center">
+            <div class="h-64 w-64 rounded-full bg-primary opacity-75 blur-[160px] transition duration-300 dark:opacity-30"></div>
+            <div class="h-64 w-64 rounded-full bg-purple-500 opacity-75 blur-[160px] transition duration-300 dark:opacity-30"></div>
+        </div>
+        <div class="relative mt-20 px-6 sm:px-12 lg:px-0 max-w-3xl mx-auto">
+        <h1
+            class="lg:w-4/5 text-4xl font-bold text-dark dark:text-white md:text-5xl"
+        >
+        Build modern UIs with tailwind css.
+        </h1>
+        <p class="mt-6 text-gray-600 dark:text-gray-300">
+            Easy to customize User Interface components, blocks and boilerplates shaped to meet your need, User centered and designed following modern web best practices.
+        </p>
+        </div>
+    </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './assets/tailus.css'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
